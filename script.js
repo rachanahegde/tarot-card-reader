96e1915821128f787e14918c7b9f4a4b64040c99
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2025-02-05 21:41:13 +0000

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Insert the 78 card backs dynamically
+// Card should shine and pop up from screen when user hovers over them
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cardContainer = document.querySelector(".card-container");
+
+  for (let i = 0; i < 78; i++) {
+    const card = document.createElement("img");
+    card.src = "../assets/cards/tarot_card_back.png";
+    card.alt = "Tarot Card Back";
+    card.className =
+      "w-[100px] transition-transform duration-100 hover:scale-110 hover:shadow-[0px_0px_5px_4px_rgba(191,145,255,0.8)] cursor-pointer";
+    cardContainer.appendChild(card);
+  }
+});
