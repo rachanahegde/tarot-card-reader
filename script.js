28c17b0508fc2b4fedc6e8691ba009babc2fd83c
Rachana Hegde <66835338+rachanahegde@users.noreply.github.com>
2025-02-06 10:31:12 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Insert the 78 card backs dynamically
+// Insert the 78 card backs dynamically (making sure that they overlap)
 // Card should shine and pop up from screen when user hovers over them
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -9,7 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
     card.src = "../assets/cards/tarot_card_back.png";
     card.alt = "Tarot Card Back";
     card.className =
-      "w-[100px] transition-transform duration-100 hover:scale-110 hover:shadow-[0px_0px_5px_4px_rgba(191,145,255,0.8)] cursor-pointer";
+      "w-[80px] -ml-8 transition-transform duration-100 hover:scale-110 hover:z-10 hover:shadow-[0px_0px_5px_4px_rgba(191,145,255,0.8)] cursor-pointer";
+
     cardContainer.appendChild(card);
   }
 });
